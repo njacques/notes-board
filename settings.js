89e84c943b8ab1cb8ed2a144793646f7a2819206
Nilson Jacques <nilson.jacques@clifton-asset.co.uk>
2020-01-12 16:05:09 +0000

    Rename ENV to more commonly used NODE_ENV

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ require('dotenv').config({silent: true});
 
 module.exports = {
   port: process.env.PORT || 3000,
-  env: process.env.ENV || 'development',
+  env: process.env.NODE_ENV || 'development',
 
   // Environment-dependent settings
   development: {
