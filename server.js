7a4fc63c4005b1e703c19a85f21214e0726cd29b
Nilson Jacques <neil.jacques@gmail.com>
2020-02-02 16:41:22 +0000

    Swap out concise.css for Bulma

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
-const Path = require('path');
-const Settings = require('./settings');
-const Routes = require('./lib/routes');
-const Models = require('./lib/models/');
+const Hapi = require("@hapi/hapi");
+const Path = require("path");
+const Settings = require("./settings");
+const Routes = require("./lib/routes");
+const Models = require("./lib/models/");
 
 const init = async () => {
-  const server = new Hapi.Server({port: Settings.port});
+  const server = new Hapi.Server({ port: Settings.port });
 
-  await server.register([
-    require('vision'),
-    require('inert')
-  ]);
+  await server.register([require("@hapi/vision"), require("@hapi/inert")]);
 
   server.views({
-    engines: {pug: require('pug')},
-    path: Path.join(__dirname, 'lib/views'),
+    engines: { pug: require("pug") },
+    path: Path.join(__dirname, "lib/views"),
     compileOptions: {
       pretty: false
     },
-    isCached: Settings.env === 'production'
+    isCached: Settings.env === "production"
   });
 
   // Add routes
@@ -32,7 +29,7 @@ const init = async () => {
   console.log(`Server running at: ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", err => {
   console.log(err);
   process.exit(1);
 });
--- static/public/scripts/main.js
+++ static/public/scripts/main.js
@@ -1,33 +1,60 @@
-MicroModal.init();
+// Modal
 
-// Cancel default action on modal trigger links to avoid changing URL
-document
-    .querySelector('[data-micromodal-trigger]')
-    .addEventListener('click', e => e.preventDefault());
+const modal = document.querySelector(".modal");
+const html = document.querySelector("html");
 
-const form = document.querySelector('#note-form');
-const url = form.getAttribute('action');
-const method = form.getAttribute('method');
+const showModal = () => {
+  modal.classList.add("is-active");
+  html.classList.add("is-clipped");
+};
+
+const hideModal = () => {
+  modal.classList.remove("is-active");
+  html.classList.remove("is-clipped");
+};
+
+document.querySelector("a.show-modal").addEventListener("click", function(e) {
+  e.preventDefault();
+  showModal();
+});
+
+modal.querySelector(".modal .delete").addEventListener("click", function(e) {
+  e.preventDefault();
+  hideModal();
+});
+
+// Form submition
+
+const form = document.querySelector("#note-form");
+const url = form.getAttribute("action");
+const method = form.getAttribute("method");
 
 const prependNote = html => {
-  const notesList = document.querySelector('.notes-list');
-  const div = document.createElement('div');
+  const notesList = document.querySelector(".notes-list");
+  const div = document.createElement("div");
   div.innerHTML = html;
   notesList.insertBefore(div.firstChild, notesList.firstChild);
 };
 
+const updateNote = html => {
+  const article = document.querySelector("article");
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  article.parentNode.replaceChild(div.firstChild, article);
+};
+
 const onSuccess = html => {
-  MicroModal.close('note-modal');
+  hideModal();
   form.reset();
 
-  if (method === 'POST') {
+  if (method === "POST") {
     prependNote(html);
-  } else if (method === 'PUT') {
-    document.querySelector('.note-content').innerHTML = html;
+  } else if (method === "PUT") {
+    updateNote(html);
   }
 };
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", e => {
   e.preventDefault();
 
   fetch(url, {
